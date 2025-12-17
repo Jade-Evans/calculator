90322e83c76644ccd41e653ab262f177a1979b5f
Jade-Evans <jpgabb@msn.com>
2025-12-17 14:49:20 +0000

    feat(JS): Create references to HTML elements for DOM Manipulation

---
+++ script.js
@@ -0,0 +1,10 @@
+const calculatorBody = document.querySelector("#calculatorBody");
+const screen = document.querySelector("#screen");
+const operatorButtonsContainer = document.querySelector("#operatorButtons");
+const operatorButtons = operatorButtonsContainer.querySelectorAll(button);
+const numbersContainer = document.querySelector("#numbersContainer");
+const numberButtons = numbersContainer.querySelectorAll(button);
+const enterButton = document.querySelector("#enter");
+const clearButton = document.querySelector("#clear");
+       
+            
