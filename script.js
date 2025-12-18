e9886bd5d01f26e4cab2f3afacd22dc02b7b0139
Jade-Evans <jpgabb@msn.com>
2025-12-18 10:37:35 +0000

    debug(numberButtons and operatorButtons): create arrays from these NodeLists to use in event listeners

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const calculatorBody = document.querySelector("#calculatorBody");
 const screen = document.querySelector("#screen");
 const operatorButtonsContainer = document.querySelector("#operatorButtons");
-const operatorButtons = operatorButtonsContainer.querySelectorAll(button);
+const operatorButtons = document.querySelectorAll("#operatorButtons button");
+const operatorButtonsArr = Array.from(operatorButtons);
 const numbersContainer = document.querySelector("#numbersContainer");
-const numberButtons = numbersContainer.querySelectorAll(button);
+const numberButtons = document.querySelectorAll("#numbersContainer button");
+const numberButtonsArr = Array.from(numberButtons);
 const enterButton = document.querySelector("#enter");
 const clearButton = document.querySelector("#clear");
        
-            
+ numberButtonsArr.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        screen.value += button.textContent;
+        console.log(button.textContent);
+    })
+
+ }
+)           
