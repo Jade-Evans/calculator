23f2824a299a064ff1169886e0b7ea5eefaca734
Jade-Evans <jpgabb@msn.com>
2025-12-18 14:33:53 +0000

    feat(equalsButton EL): create EL that splits the screenString sum and assigns n1, operator and n2, and use switch statement to assign correct equations based on the value of op

--- script.js
+++ script.js
@@ -10,36 +10,62 @@ const equalsButton = document.querySelector("#equals");
 const clearButton = document.querySelector("#clear");
 const backButton = document.querySelector("#back");
 const operators = "+-X%";
-let screenString = "";
 
-//NUMBERS/OPERATORS APPEAR ON SCREEN WHEN CLICKED//      
-numberButtonsArr.forEach(button=>{
-    button.addEventListener("click",()=>{
-        screen.value += button.textContent;
-        screenString += button.textContent;
-        console.log(`${screenString}: screenString`);
-    })
-});
-operatorButtonsArr.forEach(button=>{ 
-    button.addEventListener("click",()=>{
-        let prevButton = screenString [screenString.length -1];
-        if(!operators.includes(prevButton)){
-             screenString += button.textContent;
-             screen.value += button.textContent;
+const calculate = function(){
+    let screenString = "";
+    //NUMBERS/OPERATORS APPEAR ON SCREEN WHEN CLICKED//      
+    numberButtonsArr.forEach(button=>{
+        button.addEventListener("click",()=>{
+            screen.value += button.textContent;
+            screenString += button.textContent;
+            console.log(`${screenString}: screenString`);
+        })
+    });
+    operatorButtonsArr.forEach(button=>{ 
+        button.addEventListener("click",()=>{
+            let prevButton = screenString [screenString.length -1];
+            if(!operators.includes(prevButton)){
+                screenString += button.textContent;
+                screen.value += button.textContent;
+            }
+        })
+    });
+
+    //BACK BUTTON DELETES PREVIOUS BUTTON ON SCREEN//
+    backButton.addEventListener("click", ()=>{
+        let screenStringArr = screenString.split("").slice(0,-1);
+        screenString = screenStringArr.join("");
+        console.log(screenString);
+        screen.value = screen.value.split("").slice(0,-1).join("");
+        
+    });
+    //CLEAR BUTTON CLEARS CONTENT ON SCREEN WHEN CLICKED//          
+    clearButton.addEventListener("click",()=>{
+        screen.value = "";
+        screenString = "";
+    });
+
+    //CREATE VARIABLES FOR NUM 1, OPERATOR AND NUM 2:
+    //1 split the sum:
+    equalsButton.addEventListener("click",()=>{
+        let screenStringArr = screenString.split(" ");
+        let n1 = Number(screenStringArr[0]);
+        let op = screenStringArr[1];
+        let n2 = Number(screenStringArr[2]);
+        console.log(`${n1} = firstNumber;${op} = operator; ${n2} = secondNumber`);
+        let answer = "";
+        switch(op){
+            case "+":
+                answer = n1 + n2;
+            case "-":
+                answer = n1 - n2;
+            case "X":
+                answer = n1*n2;
+            case "%":
+                answer = n1/n2;
         }
-    })
-});
 
-//BACK BUTTON DELETES PREVIOUS BUTTON ON SCREEN//
-backButton.addEventListener("click", ()=>{
-    let screenStringArr = screenString.split("").slice(0,-1);
-    screenString = screenStringArr.join("");
-    console.log(screenString);
-    screen.value = screen.value.split("").slice(0,-1).join("");
-    
-});
-//CLEAR BUTTON CLEARS CONTENT ON SCREEN WHEN CLICKED//          
-clearButton.addEventListener("click",()=>{
-    screen.value = "";
-    screenString = "";
-});
+    })
+
+};
+calculate();
